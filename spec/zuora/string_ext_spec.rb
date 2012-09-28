9798ebf00c7de59d3fe3551f6b3f526a2b96d4f1
maksim demin <mdemin914@gmail.com>
2012-09-27 22:19:23 -0700

    fixed spec

--- spec/zuora/string_ext_spec.rb
+++ spec/zuora/string_ext_spec.rb
@@ -4,6 +4,6 @@ describe String do
   it "provides a zuora compatible camelize method" do
     "sso_id__c".zuora_camelize.should eq "SsoId__c"
     "SsoId__c".underscore.zuora_camelize.should eq "SsoId__c"
-    "some__c_other_thing_c".zuora_camelize.should eq "SomeCOtherThingC"
+    "some__c_other_thing_c".zuora_camelize.should eq "Some_cOtherThingC"
   end
 end
