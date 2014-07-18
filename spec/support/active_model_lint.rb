36514f71c5c7b6e77f9a68c2528e0a58749460a4
Ben Povlich <bpovlich@backupify.com>
2014-07-17 22:15:46 -0400

    WIP for rails_4 upgrade.

--- spec/support/active_model_lint.rb
+++ spec/support/active_model_lint.rb
@@ -1,8 +1,8 @@
 shared_examples_for "ActiveModel" do
-  require 'test/unit/assertions'
   require 'active_model/lint'
-  include Test::Unit::Assertions
+  require 'minitest/autorun'
   include ActiveModel::Lint::Tests
+  include MiniTest::Assertions
 
   ActiveModel::Lint::Tests.public_instance_methods.map { |method| method.to_s }.grep(/^test/).each do |method|
     example(method.gsub('_', ' ')){ send method }
