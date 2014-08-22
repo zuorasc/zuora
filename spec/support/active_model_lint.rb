9f8c4e447ac6651eeae40ea5b464aa6c2aec4fa3
Josh Bodah <jbodah@backupify.com>
2014-08-22 16:15:59 -0400

    delegate to RSpec instead of hacking with Minitest

--- spec/support/active_model_lint.rb
+++ spec/support/active_model_lint.rb
@@ -1,21 +1,12 @@
 shared_examples_for "ActiveModel" do
   require 'active_model/lint'
-  require 'minitest/autorun'
-  require 'minitest/assertions'
   include ActiveModel::Lint::Tests
-  # include Minitest::Assertions
 
   ActiveModel::Lint::Tests.public_instance_methods.map { |method| method.to_s }.grep(/^test/).each do |method|
-    # require 'pry'; binding.pry
-    puts "responds to assert? #{respond_to? :assert}"
     example(method.gsub('_', ' ')){ send method }
   end
 
   def model
     subject
-  end
-
-  def assert(thing_to_be_asserted, message = nil)
-    thing_to_be_asserted.should be_true
   end
 end
