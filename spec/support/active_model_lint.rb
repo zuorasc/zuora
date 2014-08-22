2e402a944a7cefce98746af7a99a9362b63373bc
Josh Bodah <jbodah@backupify.com>
2014-08-22 15:40:04 -0400

    fix some errors

--- spec/support/active_model_lint.rb
+++ spec/support/active_model_lint.rb
@@ -2,9 +2,11 @@ shared_examples_for "ActiveModel" do
   require 'active_model/lint'
   require 'minitest/autorun'
   include ActiveModel::Lint::Tests
-  include MiniTest::Assertions
+  include Minitest::Assertions
 
   ActiveModel::Lint::Tests.public_instance_methods.map { |method| method.to_s }.grep(/^test/).each do |method|
+    #require 'pry'; binding.pry
+    puts "responds to assert? #{respond_to? :assert}"
     example(method.gsub('_', ' ')){ send method }
   end
 
